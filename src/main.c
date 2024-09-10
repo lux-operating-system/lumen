250959158b8c0ce0c396ec8b858c7594d468ac8f
jewelcodes <omarmelghoul01@gmail.com>
2024-09-10 19:57:38 -0400

    main: refactor to use liblux

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
  * lumen: router enabling communication between the kernel and the servers
  */
 
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/lux/lux.h>        // execrdv
+#include <liblux/liblux.h>
 
 /* socket descriptors for the kernel connection and for the lumen server */
 int kernelsd, lumensd;
@@ -35,34 +36,25 @@ pid_t launchServer(const char *name) {
 int main(int argc, char **argv) {
     // this is the first process that runs when the system boots
     // start by establishing a socket connection with the kernel
-    self = getpid();
-
-    struct sockaddr_un kernel;
-    kernel.sun_family = AF_UNIX;
-    strcpy(kernel.sun_path, "lux:///kernel");   // special path, not a true file
-
-    struct sockaddr_un lumen;
-    lumen.sun_family = AF_UNIX;
-    strcpy(lumen.sun_path, "lux:///lumen");
-
-    kernelsd = socket(AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK, 0);  // lumen doesn't block
-    if(kernelsd < 0) return -1;     // we don't have any output device yet at this stage
-
-    // and connect to the kernel
-    int status = connect(kernelsd, (const struct sockaddr *)&kernel, sizeof(struct sockaddr_un));
-    if(status) return -1;
+    luxInitLumen();
 
     // open another socket for the lumen server
+    struct sockaddr_un lumen;
+    lumen.sun_family = AF_UNIX;
+    strcpy(lumen.sun_path, SERVER_LUMEN_PATH);
+
     lumensd = socket(AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK, 0);
     if(lumensd < 0) return -1;
 
     // bind local address
-    status = bind(lumensd, (const struct sockaddr *)&lumen, sizeof(struct sockaddr_un));
+    int status = bind(lumensd, (const struct sockaddr *)&lumen, sizeof(struct sockaddr_un));
     if(status) return -1;
 
     // set up lumen as a listener
     status = listen(lumensd, 0);    // use default backlog
     if(status) return -1;
+
+    luxLog(KPRINT_LEVEL_DEBUG, "starting launch of lumen core servers...\n");
 
     // now begin launching the servers -- these ones will be hard coded because
     // they are necessary for everything and must be located on the ramdisk as
