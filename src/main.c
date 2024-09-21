77f16edb814eaaf4cd289e8eb4785ef5954db5cb
jewelcodes <omarmelghoul01@gmail.com>
2024-09-21 17:21:44 -0400

    lumen: peek at messages to read actual size

--- src/main.c
+++ src/main.c
@@ -132,8 +132,21 @@ int main(int argc, char **argv) {
     kernelsd = luxGetKernelSocket();
     while(1) {
         // receive requests from the kernel and responses from other servers here
-        ssize_t s = recv(kernelsd, req, SERVER_MAX_SIZE, 0);
+        ssize_t s = recv(kernelsd, req, SERVER_MAX_SIZE, MSG_PEEK);     // peek to check size
         if(s > 0 && s < SERVER_MAX_SIZE) {
+            if(req->header.length > SERVER_MAX_SIZE) {
+                void *newptr = realloc(req, req->header.length);
+                if(!newptr) {
+                    luxLogf(KPRINT_LEVEL_ERROR, "failed to allocate memory for message handling\n");
+                    exit(-1);
+                }
+
+                req = newptr;
+            }
+
+            // and read the actual size
+            recv(kernelsd, req, req->header.length, 0);
+
             // request from the kernel
             if((req->header.command < 0x8000) || (req->header.command > MAX_SYSCALL_COMMAND))
                 luxLogf(KPRINT_LEVEL_WARNING, "unimplemented syscall request 0x%X len %d from pid %d\n", req->header.command,req->header.length, req->header.requester);
@@ -141,8 +154,20 @@ int main(int argc, char **argv) {
                 relaySyscallRequest(req);
         }
 
-        s = recv(vfs, res, SERVER_MAX_SIZE, 0);
+        s = recv(vfs, res, SERVER_MAX_SIZE, MSG_PEEK);  // peek to check size
         if(s > 0 && s < SERVER_MAX_SIZE) {
+            if(res->header.length > SERVER_MAX_SIZE) {
+                void *newptr = realloc(res, res->header.length);
+                if(!newptr) {
+                    luxLogf(KPRINT_LEVEL_ERROR, "failed to allocate memory for message handling\n");
+                    exit(-1);
+                }
+
+                res = newptr;
+            }
+
+            recv(vfs, res, res->header.length, 0);
+
             // response from the vfs
             if((res->header.command < 0x8000) || (res->header.command > MAX_SYSCALL_COMMAND))
                 luxLogf(KPRINT_LEVEL_WARNING, "unimplemented syscall response 0x%X len %d for pid %d\n", res->header.command,res->header.length, res->header.requester);
